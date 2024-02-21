22541ccd709dc5ff03b103d8e688f6ea6d0937fc
João Paulo <joaopaulomayka@gmail.com>
2024-02-21 16:51:49 -0300

    form scope

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
+import Form from "./components/Form";
 
 function App() {
-  
-
   return (
-   <div>
-    <h1 className="text-red-600">
-      Olá Mundo!
-    </h1>
-   </div>
-  )
+    <div>
+      <h1 className="text-red-600">Inscreva-se</h1>
+      <p>Assine nossa Newsletter e mantenha-se informado</p>
+      <div>
+        <Form/>
+      </div>
+      <p>Ao se inscrever, você passará a receber os nossos e-mails com as melhores dicas, novidades e ofertas</p>
+    </div>
+  );
 }
 
-export default App
+export default App;
